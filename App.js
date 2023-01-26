7431bcdf14eb8d64013c68d7b3fd87d7204c40ae
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-01-26 09:48:14 -0300

    notificação

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import * as Notifications from "expo-notifications";
+/* manipulador de eventos de noticidcação */
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    /* Necessário para IOS */
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+          allowAnnouncements: true,
+        },
+      });
+    }
+    permissoesIos();
+
+    /* Ouvinte de evento para as notificações recebidas, ou seja, q
+    uando a notificação aparece no topo da tela do dispositivo. */
+    Notifications.addNotificationReceivedListener((notificacao) => {
+      console.log(notificacao);
+    });
+
+    /* Ouvinte de evento para as respostas dadas às notificações, ou seja
+    quando o usuário interage (toca) na notificação. */
+    Notifications.addNotificationResponseReceivedListener((resposta) => {
+      console.log(resposta);
+    });
+  }, []);
+
+  const enviarMensagem = async () => {
+    const mensagem = {
+      title: "Lembrete!",
+      body: "Não esqueça de tomar água!",
+    };
+    /* Função de agendamento de notificações */
+    await Notifications.scheduleNotificationAsync({
+      content: mensagem,
+      trigger: { seconds: 5 },
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={estilos.container}>
+        <Text>Exemplo de sistema de notificação local</Text>
+        <Button title="Disparar notificação" onPress={enviarMensagem} />
+      </SafeAreaView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
